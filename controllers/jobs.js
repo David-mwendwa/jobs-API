548bad5073fb29674dbf3292c4890398bfe22dd2
David-mwendwa <klatzdavie19@gmail.com>
2022-02-12 16:29:25 +0300

    get single job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -8,7 +8,16 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send('get job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
