518c2a602506662b7830d1640b44c35ee1801061
David-mwendwa <klatzdavie19@gmail.com>
2022-02-12 18:11:28 +0300

    handle cast errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -14,12 +14,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     )} field, please choose another value`;
     customError.statusCode = 400;
   }
-  
+
   if (err.name === 'ValidationError') {
     customError.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(', ');
     customError.statusCode = 400;
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`;
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
