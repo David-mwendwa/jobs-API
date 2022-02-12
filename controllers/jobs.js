b4bb3bbc4008dc1557f796463d9c9749863f940c
David-mwendwa <klatzdavie19@gmail.com>
2022-02-12 15:59:39 +0300

    create job route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,3 +1,7 @@
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors');
+
 const getAllJobs = async (req, res) => {
   res.send('get all jobs');
 };
@@ -7,7 +11,9 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({job});
 };
 
 const updatedJob = async (req, res) => {
