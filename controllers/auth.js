3447716418fceff5a78e68454bba52ca48ff0a5b
David-mwendwa <klatzdavie19@gmail.com>
2022-02-12 14:14:34 +0300

    set up login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -8,7 +9,16 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError('Invalid credentials');
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -32,9 +32,13 @@ UserSchema.pre('save', async function (next) {
 });
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, 'jwtSecret', {
-    expiresIn: '30d',
-  });
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFTETIME,
+    }
+  );
 };
 
 module.exports = mongoose.model('User', UserSchema);
